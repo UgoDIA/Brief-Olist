1d0fb8aa3974bc5e9ae9849397647277a3ad0ab0
UgoDIA <112081131+UgoDIA@users.noreply.github.com>
2023-04-06 08:58:53 +0200

    suppr authentification

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,7 @@
-from controllers    import authentification
+
 from hug.middleware import CORSMiddleware
 import hug
 
 api = hug.API(__name__)
 api.http.add_middleware(CORSMiddleware(api, allow_origins=['*'])) # allow_origins à restreindre pour le déploiement
 
-api.extend(authentification, '/api/authentication')

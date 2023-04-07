fe858c87c326b5f5af9a4e2d12cbc29eebe2071c
lucas <lucas.lvn97439@gmail.com>
2023-04-07 15:23:27 +0400

    server cors problem résolue

--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,8 @@ import hug
 
 from controllers.controllers import map, evolutions
 
-router = hug.route.API(__name__)
+api = hug.API(__name__)
+api.http.add_middleware(CORSMiddleware(api, allow_origins=['*'])) # allow_origins à restreindre pour le déploiement
 
-router.get('/api/map')(map.getData)
-router.get('/api/evolutions')(evolutions.getDatas)
+hug.get('/api/map', api=api)(map.getData)
+hug.get('/api/evolutions', api=api)(evolutions.getDatas)

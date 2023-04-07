d2fbc2278a716db0bfc7db9294fbb761a9eb1da2
Quentin PAYET <quentinpayet1203@gmail.com>
2023-04-07 10:45:31 +0400

    formatage datas pour highchart

--- backend/controllers/mapDataController.py
+++ backend/controllers/mapDataController.py
@@ -5,5 +5,22 @@ from sqlalchemy import func
 
 def mapDataController():
     datas = session.query(Customers.state, func.sum(OrderItems.price * OrderItems.qty)).join(Orders, Orders.customer == Customers.id).join(OrderItems, OrderItems.order == Orders.id).group_by(Customers.state)
+    # print('==================>', datas.all())
+    datas = datas.all()
+    datasList = []
+    for elt in datas:
+        # print(elt)
+        i = 1
+        for subelt in elt:
+            if i == 1:
+                state = 'br-' +  subelt.lower()
+                # print(state)
+                i+=1
+            elif i == 2:
+                valueList = [state, round(subelt, 2)]
+                datasList.append(valueList)
+                # print(valueList)
+                i = 1
+    print(datasList)
 
-    return datas
+    return datasList

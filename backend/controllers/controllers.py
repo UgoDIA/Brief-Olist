a8ffe22bdfeef0b2b6f93a17781ff73439ee04f0
Quentin PAYET <quentinpayet1203@gmail.com>
2023-04-07 15:33:29 +0400

    indentations

--- backend/controllers/controllers.py
+++ backend/controllers/controllers.py
@@ -5,20 +5,20 @@ from sqlalchemy import func, desc, types
  
 class map:
     def getData():
-        datas =  session.query(Customers.state, func.sum(OrderItems.price * OrderItems.qty)).join(Orders, Orders.customer == Customers.id).join(OrderItems, OrderItems.order == Orders.id).group_by(Customers.state).all()
+        datas =  session.query(Customers.state, func.sum(OrderItems.price * OrderItems.qty))\
+            .join(Orders, Orders.customer == Customers.id)\
+            .join(OrderItems, OrderItems.order == Orders.id)\
+            .group_by(Customers.state).all()
         datasList = []
         for elt in datas:
-            # print(elt)
             i = 1
             for subelt in elt:
                 if i == 1:
                     state = 'br-' +  subelt.lower()
-                    # print(state)
                     i+=1
                 elif i == 2:
                     valueList = [state, round(subelt, 2)]
                     datasList.append(valueList)
-                    # print(valueList)
                     i = 1
         return datasList
 class evolutions:

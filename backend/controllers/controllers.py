9fb2f17aff09fc44031d287b0c4d671d8a94a98c
Quentin0312 <112090824+Quentin0312@users.noreply.github.com>
2023-04-16 09:49:50 +0400

    format datas for chartjs

--- backend/controllers/controllers.py
+++ backend/controllers/controllers.py
@@ -1,11 +1,15 @@
 from database.database import session
 from database.models import Customers, OrderItems, Orders
+import re
+
+import datetime
 
 from sqlalchemy import func, desc, types
- 
+
+
 class map:
     def getData():
-        datas =  session.query(Customers.state, func.sum(OrderItems.price * OrderItems.qty))\
+        datas = session.query(Customers.state, func.sum(OrderItems.price * OrderItems.qty))\
             .join(Orders, Orders.customer == Customers.id)\
             .join(OrderItems, OrderItems.order == Orders.id)\
             .group_by(Customers.state).all()
@@ -14,15 +18,32 @@ class map:
             i = 1
             for subelt in elt:
                 if i == 1:
-                    state = 'br-' +  subelt.lower()
-                    i+=1
+                    state = 'br-' + subelt.lower()
+                    i += 1
                 elif i == 2:
                     valueList = [state, round(subelt, 2)]
                     datasList.append(valueList)
                     i = 1
         return datasList
+
+
 class evolutions:
     def getDatas(region=None, annee=None):
+        def chartJSFormater(datas):
+            labels = []
+            values = []
+            for elt in datas:
+                for subelt in elt:
+                    if isinstance(subelt, str):
+                        labels.append(subelt)
+                    elif isinstance(subelt, datetime.datetime):
+                        pattern = r'(\d{4}-\d{2})'
+                        match = re.search(pattern, str(subelt))
+                        labels.append(match.group(1))
+                    elif isinstance(subelt, int) or isinstance(subelt, float):
+                        values.append(round(subelt, 2))
+            return [labels, values]
+
         def getTOP10product(region, annee):
             # Si pas de régions et pas d'années séléctionné
             if region == None and annee == None:
@@ -35,12 +56,12 @@ class evolutions:
             # Si seulement région séléctionné
             elif annee == None:
                 # Ne fonctionnait pas, cause => order_by , pq => import les class dans les modele qd fk
-                datas = session.query(OrderItems.product,Customers.state, func.sum(OrderItems.qty).label('qtySum'))\
+                datas = session.query(OrderItems.product, Customers.state, func.sum(OrderItems.qty).label('qtySum'))\
                     .join(Orders, Orders.customer == Customers.id).join(OrderItems, OrderItems.order == Orders.id)\
                     .where(Customers.state == region)\
                     .group_by(OrderItems.product, Customers.state)\
                     .order_by(desc('qtySum')).limit(10)
-                
+
                 # sql = text(
                 #     "SELECT order_items.product, sum(order_items.qty) AS qtySum \
                 #     FROM customers JOIN orders ON orders.customer = customers.id JOIN order_items ON order_items.\"order\" = orders.id \
@@ -56,7 +77,7 @@ class evolutions:
             # Si seulement annee séléctionné
             elif region == None:
                 return 'todo'
-            
+
             # Régions et année séléctionné
             else:
                 return 'todo'
@@ -71,32 +92,36 @@ class evolutions:
                 return datas
             else:
                 return 'todo'
-        
+
+        # ATTENTION Ici les valeurs contenu qui n'ont pas de date (null) ne sont pas envoyé
+        # à cause de 'isinstance(date)' dans le formateurChartJS
         def getEvolutionsRegionCA(region, annee):
             if region == None and annee == None:
-                datas = session.query(func.date_trunc('month',Orders.delivered_customer_date).label("test"), func.sum(OrderItems.price * OrderItems.qty))\
+                datas = session.query(func.date_trunc('month', Orders.delivered_customer_date).label("test"), func.sum(OrderItems.price * OrderItems.qty))\
                     .join(OrderItems, OrderItems.order == Orders.id)\
                     .group_by("test")\
                     .order_by(desc('test'))
                 return datas
-            
+
             elif annee == None:
-                datas = session.query(Customers.state,func.date_trunc('month',Orders.delivered_customer_date).label("test"), func.sum(OrderItems.price * OrderItems.qty))\
+                datas = session.query(Customers.state, func.date_trunc('month', Orders.delivered_customer_date).label("test"), func.sum(OrderItems.price * OrderItems.qty))\
                     .join(Orders, Orders.customer == Customers.id)\
                     .join(OrderItems, OrderItems.order == Orders.id)\
                     .group_by(Customers.state, "test").where(Customers.state == region)\
                     .order_by(desc('test'))
                 return datas
-            
+
             elif region == None:
                 return 'todo'
 
         dicoDatas = {}
 
-        dicoDatas['TOP10product'] = getTOP10product(region, annee)
+        dicoDatas['TOP10product'] = chartJSFormater(
+            getTOP10product(region, annee))
 
-        dicoDatas['TOP10states'] = getTOP10states(annee)
+        dicoDatas['TOP10states'] = chartJSFormater(getTOP10states(annee))
 
-        dicoDatas['evolutionsRegionsCA'] = getEvolutionsRegionCA(region, annee)
+        dicoDatas['evolutionsRegionsCA'] = chartJSFormater(
+            getEvolutionsRegionCA(region, annee))
 
         return dicoDatas

e19f51537836899988323f0d17daa85f33a9e3de
lucas.lvn <lucas.lvn97439@gmail.com>
2023-05-01 14:37:14 +0200

    ajout d'une url pour récuperer une range d'année

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from hug.middleware import CORSMiddleware
 import hug
 
-from controllers.controllers import map, evolutions, annonce
+from controllers.controllers import map, evolutions, annonce, data
 
 api = hug.API(__name__)
 # allow_origins à restreindre pour le déploiement
@@ -10,3 +10,4 @@ api.http.add_middleware(CORSMiddleware(api, allow_origins=['*']))
 hug.get('/api/map', api=api)(map.getData)
 hug.get('/api/evolutions', api=api)(evolutions.getDatas)
 hug.get('/api/annonces', api=api)(annonce.getDatas)
+hug.get('/api/date/minmax', api=api)(data.MinMaxYear)
